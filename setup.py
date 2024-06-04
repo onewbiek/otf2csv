ad131bef1dc596cb84c4802e3706cf4d72db302c
onewbiek <yankun0213@gmail.com>
2024-06-04 11:05:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="otf2_parser",
+    name="otf2-parser",
     version="0.1",
     author="Onewbiek",
     author_email="yankun0213@gmail.com",
     description="An otf2 parser that convert otf2 to csv",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/onewbiek/otf2csv",
+    url="https://github.com/onewbiek/otf2parser",
     packages=['otf2_parser'],                  # package for import: after installaion, import otf2_parser
     install_requires=[
         "otf2",
